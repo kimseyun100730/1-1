fc74ffdaac6bd8365563c508ef3442a8443a20e8
kimseyun100730 <77229097+kimseyun100730@users.noreply.github.com>
2023-07-27 12:57:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import './App.css';
 
 function App() {
   const [studentId, setStudentId] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleLogin = () => {
-    // 로그인 처리
-    // 실제로는 서버와 통신하여 학생 정보를 확인해야 함
-    // 여기서는 학생 정보를 하드코딩하고 비밀번호가 "0000"인 경우 로그인 성공으로 가정
-    if (studentId.startsWith('mj101') && password === '0000') {
-      setError('');
-      // 로그인 성공 시, 원하는 화면으로 이동 (예시로 로그인 성공 메시지 출력)
-      alert('로그인 성공!');
+    // 서버에 학생 정보와 비밀번호를 전달하여 로그인 처리를 수행
+    // 예시로는 간단하게 학생 아이디와 비밀번호를 하드코딩으로 비교
+    if (studentId === 'mj10101' && password === '0000') {
+      // 로그인 성공 시 페이지 이동 또는 알림장 페이지를 보여주는 로직
+      // 여기서는 예시로 콘솔에 로그인 성공 메시지를 출력
+      console.log('로그인 성공');
     } else {
-      setError('로그인 실패. 학번과 비밀번호를 확인하세요.');
+      // 로그인 실패 시 에러 메시지 출력
+      setErrorMessage('아이디 또는 비밀번호가 올바르지 않습니다.');
     }
   };
 
   return (
     <div className="App">
-      <div className="login-box">
-        <img src="school_logo.png" className="school-logo" alt="School Logo" />
-        <h1>학교 알림장</h1>
-        <div className="form">
-          <label>학번:</label>
-          <input type="text" value={studentId} onChange={(e) => setStudentId(e.target.value)} />
-
-          <label>비밀번호:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-          <button onClick={handleLogin}>로그인</button>
-        </div>
-        {error && <p className="error-message">{error}</p>}
+      <div className="login-container">
+        <h1>알림장 로그인</h1>
+        <input
+          type="text"
+          placeholder="학생 아이디"
+          value={studentId}
+          onChange={(e) => setStudentId(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>로그인</button>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
       </div>
     </div>
   );
