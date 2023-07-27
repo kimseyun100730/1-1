24e7f5a464955c5ada48289356a4db8cece393b9
kimseyun100730 <77229097+kimseyun100730@users.noreply.github.com>
2023-07-27 12:29:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# 학생 정보를 저장할 임시 데이터 (예시로 딕셔너리를 사용)
+students = {
+    "mj10101": {"name": "홍길동", "password": "0000", "class": "1-1"},
+    "mj10102": {"name": "김철수", "password": "0000", "class": "1-1"},
+    # 학생 정보 추가...
+}
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', error=None)
 
 @app.route('/index2', methods=['POST'])
 def index2():
     student_id = request.form['student_id']
     password = request.form['password']
 
-    # 학생 정보를 확인하고 로그인 실패 시 오류 메시지를 error 변수에 저장
     if student_id in students and students[student_id]['password'] == password:
         student_info = students[student_id]
         return render_template('index2.html', student=student_info)
