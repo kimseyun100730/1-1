b436a4dbbedda93a8e8daa88f466b0fa9f19c4ef
kimseyun100730 <77229097+kimseyun100730@users.noreply.github.com>
2023-07-27 12:01:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 # 학생 정보를 저장할 임시 데이터 (예시로 딕셔너리를 사용)
 students = {
-    "mj10101": {"name": "김보민", "password": "0000", "class": "1-1"},
-    "mj10102": {"name": "10102", "password": "0000", "class": "1-1"},
+    "mj10101": {"name": "홍길동", "password": "0000", "class": "1-1"},
+    "mj10102": {"name": "김철수", "password": "0000", "class": "1-1"},
     # 학생 정보 추가...
 }
 
@@ -23,7 +23,8 @@ def index2():
         student_info = students[student_id]
         return render_template('index2.html', student=student_info)
     else:
-        return "로그인 실패. 학번과 비밀번호를 확인하세요."
+        error_message = "로그인 실패. 학번과 비밀번호를 확인하세요."
+        return render_template('index.html', error=error_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
